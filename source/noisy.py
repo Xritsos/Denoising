22960f3af60612e2a93e79eafb5d9993bc582db5
Xritsos <kobecris24@yahoo.gr>
2024-04-22 22:01:04 +0300

    added random seed for noise

--- source/noisy.py
+++ source/noisy.py
@@ -1,7 +1,8 @@
-
-
+import torch
 
 def add_noise(inputs,noise_factor=0.3):
+    torch.manual_seed(7)
+    
 	noisy = inputs+torch.randn_like(inputs) * noise_factor
 	noisy = torch.clip(noisy,0.,1.)
 	return noisy
