6766a395c77d003b615fe27c27f0c4c37ab6d86a
Xritsos <kobecris24@yahoo.gr>
2024-04-22 21:39:02 +0300

    made sure everything is on gpu

--- source/train_val_test.py
+++ source/train_val_test.py
@@ -6,14 +6,16 @@ LINE_UP = '\033[1A'
 LINE_CLEAR = '\x1b[2K'
 
 def train(net, train_loader, optimizer, loss_fn, device):
-    running_loss = 0.0
+    running_loss = torch.tensor([0.0]).to(device)
     net.train()
     step = 0
     for img, _ in train_loader:
         img = img.to(device)
+        # image_noisy = add_noise(image_batch,noise_factor)
+        # image_noisy = image_noisy.to(device)
         optimizer.zero_grad()
-        output = net(img)
-        loss = loss_fn(output, img)
+        output = net(img).to(device)
+        loss = loss_fn(output, img).to(device)
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
@@ -25,7 +27,7 @@ def train(net, train_loader, optimizer, loss_fn, device):
     
     train_loss = running_loss / len(train_loader)
     
-    print(f"Train Step {step}/{len(train_loader)} --- Train Loss: {train_loss}")
+    print(f"Train Step {step}/{len(train_loader)} --- Train Loss: {float(train_loss.cpu())}")
     
     return train_loss
 
@@ -33,14 +35,14 @@ def train(net, train_loader, optimizer, loss_fn, device):
 def val(net, val_loader, loss_fn, device):
     net.eval()
     with torch.no_grad(): 
-        running_loss = 0.0 
+        running_loss = torch.tensor([0.0]).to(device) 
         step = 0
         for img, _ in val_loader:
             img = img.to(device)
             # image_noisy = add_noise(image_batch,noise_factor)
             # image_noisy = image_noisy.to(device)
-            output = net(img)
-            loss = loss_fn(output, img)
+            output = net(img).to(device)
+            loss = loss_fn(output, img).to(device)
             running_loss += loss.item()
             step += 1
             
@@ -50,7 +52,7 @@ def val(net, val_loader, loss_fn, device):
             
         val_loss = running_loss / len(val_loader)
         
-    print(f"Val Step {step}/{len(val_loader)} ------- Val Loss:   {val_loss}")
+    print(f"Val Step {step}/{len(val_loader)} ------- Val Loss:   {float(val_loss.cpu())}")
     
     return val_loss
 
@@ -63,14 +65,14 @@ def test(net, test_loader, device):
     with torch.no_grad():
         for img, _ in test_loader:
             img = img.to(device)
-            output = net(img)
+            output = net(img).to(device)
             
             img = img[33] * STD[:, None, None] + MEAN[:, None, None]
             img = img.permute(1, 2, 0).cpu()
             
             fig, axs = plt.subplots(1, 2, figsize=(10, 10))
             axs[0].imshow(img)
-            axs[1].imshow(output[33].permute(1, 2, 0).cpu())
+            axs[1].imshow(output[33].permute(1, 2, 0).cpu().numpy())
             
             plt.show()
             exit()
