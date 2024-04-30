b722f104de1006af84026fa490778f1df028680e
Xritsos <kobecris24@yahoo.gr>
2024-04-30 22:57:04 +0300

    optimized code

--- source/noisy.py
+++ source/noisy.py
@@ -2,10 +2,10 @@ import torch
 import numpy as np
 import seaborn as sns
 from matplotlib import pyplot as plt
-from photutils.datasets import apply_poisson_noise
 
 
-def add_gaussian(img, mean=0, sigma=0.3):
+@torch.jit.script
+def add_gaussian(img, mean, sigma):
 	torch.manual_seed(7)
  
 	noisy = torch.clone(img).to(img.device)
@@ -30,37 +30,44 @@ def add_gaussian(img, mean=0, sigma=0.3):
 #     return noisy
 
 
-def add_multiplicative(img, mean=0, sigma=0.3):
+@torch.jit.script
+def add_multiplicative(img, mean, sigma):
 	torch.manual_seed(7)
 
 	noisy = torch.clone(img).to(img.device)
 	noise = torch.normal(mean=mean, std=sigma, size=noisy.shape).to(img.device)
  
-	noisy = noisy + noisy * noise
+	noisy = noisy + torch.multiply(noisy, noise)
 	noisy = torch.clip(noisy, 0., 1.)
 
 	return noisy
 
 
+@torch.jit.script
 def add_noise(batch):
 	torch.manual_seed(7)
  
 	noisy_batch = torch.clone(batch).to(batch.device)
  
+	multi_mean = torch.tensor(1.0)
+	multi_sigma = torch.tensor(0.5)
+	gauss_mean = torch.tensor(0.2)
+	gauss_sigma = torch.tensor(0.2)
+ 
 	for i in range(noisy_batch.shape[0]):
 		inputs = batch[i, :, :, :].to(batch.device)
-		index = np.random.randint(low=1, high=4, size=1)
- 
+		index = torch.randint(low=1, high=4, size=[1]).to(batch.device)
+  
 		if index == 1:
-			noisy = add_multiplicative(inputs, mean=1.0, sigma=0.5)
+			noisy = add_multiplicative(inputs, mean=multi_mean, sigma=multi_sigma)
 		elif index == 2:
-			noisy = add_gaussian(inputs, mean=0.2, sigma=0.2)
+			noisy = add_gaussian(inputs, mean=gauss_mean, sigma=gauss_sigma)
 		elif index == 3:
-			noisy = add_multiplicative(inputs, mean=1.0, sigma=0.5)
-			noisy = add_gaussian(noisy, mean=0.2, sigma=0.1)
-		elif index == 4:
-			noisy = add_gaussian(inputs, mean=0.2, sigma=0.2)
-			noisy = add_multiplicative(noisy, mean=1.0, sigma=0.2)
+			noisy = add_multiplicative(inputs, mean=multi_mean, sigma=multi_sigma)
+			noisy = add_gaussian(noisy, mean=gauss_mean, sigma=gauss_sigma)
+		else:
+			noisy = add_gaussian(inputs, mean=gauss_mean, sigma=gauss_sigma)
+			noisy = add_multiplicative(noisy, mean=multi_mean, sigma=multi_sigma)
    
 		noisy_batch[i, :, :, :] = noisy
   
