2a53813226e42e5c3b5bd7760840a8095a547737
Xritsos <kobecris24@yahoo.gr>
2024-04-30 22:58:30 +0300

    added psnr calculation

--- source/train_val_test.py
+++ source/train_val_test.py
@@ -3,12 +3,14 @@ from torchviz import make_dot
 from matplotlib import pyplot as plt
 
 from noisy import add_noise
+from noise_metrics import PSNR
 
 LINE_UP = '\033[1A'
 LINE_CLEAR = '\x1b[2K'
 
 def train(net, train_loader, optimizer, loss_fn, device):
     running_loss = torch.tensor([0.0]).to(device)
+    running_psnr = torch.tensor([0.0]).to(device)
     net.train()
     step = 0
     for img, _ in train_loader:
@@ -17,6 +19,9 @@ def train(net, train_loader, optimizer, loss_fn, device):
         image_noisy = image_noisy.to(device)
         optimizer.zero_grad()
         output = net(image_noisy).to(device)
+        
+        running_psnr += PSNR(img, output)
+        
         loss = loss_fn(output, img).to(device)
         loss.backward()
         optimizer.step()
@@ -29,7 +34,9 @@ def train(net, train_loader, optimizer, loss_fn, device):
     
     train_loss = running_loss / len(train_loader)
     
-    print(f"Train Step {step}/{len(train_loader)} --- Train Loss: {float(train_loss.cpu())}")
+    psnr_score = running_psnr / len(train_loader)
+    
+    print(f"Train Step {step}/{len(train_loader)} --- Train Loss: {float(train_loss.cpu())} ---- Train PSNR: {float(psnr_score.cpu())}")
     
     return train_loss
 
@@ -38,12 +45,16 @@ def val(net, val_loader, loss_fn, device):
     net.eval()
     with torch.no_grad(): 
         running_loss = torch.tensor([0.0]).to(device) 
+        running_psnr = torch.tensor([0.0]).to(device)
         step = 0
         for img, _ in val_loader:
             img = img.to(device)
             image_noisy = add_noise(img)
             image_noisy = image_noisy.to(device)
             output = net(image_noisy).to(device)
+            
+            running_psnr += PSNR(img, output)
+            
             loss = loss_fn(output, img).to(device)
             running_loss += loss.item()
             step += 1
@@ -54,7 +65,9 @@ def val(net, val_loader, loss_fn, device):
             
         val_loss = running_loss / len(val_loader)
         
-    print(f"Val Step {step}/{len(val_loader)} ------- Val Loss:   {float(val_loss.cpu())}")
+        psnr_score = running_psnr / len(val_loader)
+        
+    print(f"Val Step {step}/{len(val_loader)} ------- Val Loss:   {float(val_loss.cpu())} ---- Val PSNR: {float(psnr_score.cpu())}")
     
     return val_loss
 
