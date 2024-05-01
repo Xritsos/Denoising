1be61b9221f7323f7359825f1cae05e6c48e6ec9
Xritsos <kobecris24@yahoo.gr>
2024-05-01 17:36:06 +0300

    read parameters from file

--- source/main.py
+++ source/main.py
@@ -1,6 +1,7 @@
 import os
 import time
 import torch
+import pandas as pd
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
@@ -13,108 +14,149 @@ from train_val_test import train, val, test
 
 
 def main():
-    test_id = 1 # it will be read from file automatically
-    EPOCHS = 100
-    BATCH = 128
-    LR = 1e-3
-    VAL_SIZE = 5000
-    SAVE_PATH = f'/home/akahige/Python Work/Denoising/archive/model_ckpts/fully_cnn_{test_id}/'
     
-    if not os.path.exists(SAVE_PATH):
-        os.mkdir(SAVE_PATH)
-     
-    torch.manual_seed(7)
+    df = pd.read_csv('/home/akahige/Python Work/Denoising/tests_fully.csv')
     
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    test_ids = list(df['test_id'])
     
-    train_loader, val_loader, test_loader, _ = data_load(validation_size=VAL_SIZE,
-                                                         batch_size=BATCH, 
-                                                         visualize_split=False)
-    print()
-    print(f"Available Device: {device}")
-    print("Proceeding with training...")
+    for test_id in test_ids:
+        row = int(test_id - 1)
+        EPOCHS = int(df['epochs'][row])
+        BATCH = int(df['batch_size'][row])
+        LR = float(df['learning_rate'][row])
+        AMSGRAD = bool(df['amsgrad'][row])
+        VAL_SIZE = 5000
+        SAVE_PATH = f'/home/akahige/Python Work/Denoising/archive/model_ckpts/fully_cnn_{test_id}/'
     
-    model = AutoEncoder().to(device)
-    
-    # summary(model, (3, 32, 32))
-    # exit()
-    
-    loss_fn = nn.MSELoss()
-    loss_fn.to(device)
-    
-    optimizer = torch.optim.Adam(model.parameters(), lr=LR)
-    
-    train_loss = []
-    validation_loss = []
-    previous_loss = []
-    start_total_time = time.time()
-    for epoch in range(EPOCHS):
+        print("================== Parameters ====================")
+        print(f"Epochs: {EPOCHS}")
+        print(f"Batch Size: {BATCH}")
+        print(f"Learning Rate: {LR}")
+        print(f"Use AmsGrad: {AMSGRAD}")
+        print(f"Save Destination: {SAVE_PATH}")
+        print("==================================================")
+            
+        if not os.path.exists(SAVE_PATH):
+            os.mkdir(SAVE_PATH)
+        
+        torch.manual_seed(7)
+        
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
+        train_loader, val_loader, test_loader, _ = data_load(validation_size=VAL_SIZE,
+                                                             batch_size=BATCH, 
+                                                             visualize_split=False)
         print()
-        print(f"============ In Epoch {epoch+1}/{EPOCHS} ==================")
+        print(f"Available Device: {device}")
+        print("Proceeding with training...")
         
-        start_train_time = time.time()
+        model = AutoEncoder().to(device)
         
-        tr_loss = train(model, train_loader, optimizer, loss_fn, device)
+        # summary(model, (3, 32, 32))
+        # exit()
         
-        end_train_time = time.time()
-        train_loss.append(float(tr_loss.cpu()))
+        loss_fn = nn.MSELoss()
+        loss_fn.to(device)
         
-        train_time = round(end_train_time - start_train_time, 2)
+        optimizer = torch.optim.Adam(model.parameters(), lr=LR, amsgrad=AMSGRAD)
         
-        start_val_time = time.time()
-        
-        
-        val_loss = val(model, val_loader, loss_fn, device)
-        
-        end_val_time = time.time()
-        validation_loss.append(float(val_loss.cpu()))
-        
-        val_time = round(end_val_time - start_val_time, 2)
-        
-        end_epoch_time = time.time()
-        epoch_time = round(end_epoch_time - start_train_time, 2)
-        
-        if epoch == 0:
-            previous_loss.append(val_loss)
-        elif epoch > 0:
-            if val_loss < previous_loss[0]:
-                previous_loss.pop()
+        train_loss = []
+        validation_loss = []
+        train_psnr = []
+        validation_psnr = []
+        previous_loss = []
+        psnr = []
+        start_total_time = time.time()
+        for epoch in range(EPOCHS):
+            print()
+            print(f"============ In Epoch {epoch+1}/{EPOCHS} ==================")
+            
+            start_train_time = time.time()
+            
+            tr_loss, tr_psnr = train(model, train_loader, optimizer, loss_fn, device)
+            
+            end_train_time = time.time()
+            train_loss.append(float(tr_loss.cpu()))
+            train_psnr.append(float(tr_psnr.cpu()))
+            
+            train_time = round(end_train_time - start_train_time, 2)
+            
+            start_val_time = time.time()
+            
+            
+            val_loss, val_psnr = val(model, val_loader, loss_fn, device)
+            
+            end_val_time = time.time()
+            validation_loss.append(float(val_loss.cpu()))
+            validation_psnr.append(float(val_psnr.cpu()))
+            
+            val_time = round(end_val_time - start_val_time, 2)
+            
+            end_epoch_time = time.time()
+            epoch_time = round(end_epoch_time - start_train_time, 2)
+            
+            if epoch == 0:
                 previous_loss.append(val_loss)
-                
-                torch.save(model.state_dict(), f'{SAVE_PATH}{test_id}.pt')
+                psnr.append(val_psnr)
+            elif epoch > 0:
+                if val_loss < previous_loss[0]:
+                    previous_loss.pop()
+                    previous_loss.append(val_loss)
+                    
+                    psnr.pop()
+                    psnr.append(val_psnr)
+                    
+                    torch.save(model.state_dict(), f'{SAVE_PATH}{test_id}.pt')
 
+            print()
+            print(f"=========== Total Epoch Runtime {epoch_time} s ====================")
+            
+        end_total_time = time.time()
+        
+        total_time = round((end_total_time - start_total_time) / 60, 2)
+        
         print()
-        print(f"=========== Total Epoch Runtime {epoch_time} s ====================")
+        print("==========================================================")
+        print(f"========= Total Runtime {total_time} minutes !===========")
+        print("==========================================================")
         
-    end_total_time = time.time()
-    
-    total_time = round((end_total_time - start_total_time) / 60, 2)
-    
-    print()
-    print("==========================================================")
-    print(f"========= Total Runtime {total_time} minutes !===========")
-    print("==========================================================")
-    
-    print(f"Loss saved: {previous_loss[0]}")
-    
-    epochs = [i for i in range(1, EPOCHS + 1)]
-    
-    fig = plt.figure(figsize=(10, 10))
-    plt.plot(epochs, train_loss, label='Train Loss')
-    plt.plot(epochs, validation_loss, label='Validation Loss')
-    
-    plt.xlabel('Number of Epochs')
-    plt.ylabel('Loss')
-    plt.suptitle('Losses')
-    plt.legend()
-    
-    plt.savefig(f'{SAVE_PATH}{test_id}_loss.png')
-    plt.show()
+        print(f"Loss saved: {float(previous_loss[0].cpu())}")
+        print(f"PSNR saved: {float(psnr[0].cpu())}")
         
+        df.loc[row, 'Val Loss'] = float(previous_loss[0].cpu())
+        df.loc[row, 'Val PSNR'] = float(psnr[0].cpu())
         
+        df.to_csv('/home/akahige/Python Work/Denoising/tests_fully.csv', index=False)
+        
+        epochs = [i for i in range(1, EPOCHS + 1)]
+        
+        fig = plt.figure(figsize=(10, 10))
+        plt.plot(epochs, train_loss, label='Train Loss')
+        plt.plot(epochs, validation_loss, label='Validation Loss')
+        
+        plt.xlabel('Number of Epochs')
+        plt.ylabel('Loss')
+        plt.suptitle('Losses')
+        plt.legend()
+        
+        plt.savefig(f'{SAVE_PATH}{test_id}_loss.png')
+        # plt.show()
+        
+        fig = plt.figure(figsize=(10, 10))
+        plt.plot(epochs, train_psnr, label='Train PSNR')
+        plt.plot(epochs, validation_psnr, label='Validation PSNR')
+        
+        plt.xlabel('Number of Epochs')
+        plt.ylabel('PSNR (dB)')
+        plt.suptitle('PSNR Score')
+        plt.legend()
+        
+        plt.savefig(f'{SAVE_PATH}{test_id}_psnr.png')
+        # plt.show()
+          
     # load best model for testing
     # model = AutoEncoder().to(device)
-    # model.load_state_dict(torch.load(f'{SAVE_PATH}{test_id}.pt')
+    # model.load_state_dict(torch.load('/home/akahige/Python Work/Denoising/archive/model_ckpts/fully_cnn_1/1.pt')
     # test(model, test_loader, device)
     
     
