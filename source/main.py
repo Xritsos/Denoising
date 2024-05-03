087ad3285e2f73a097d43a92854b42d8ccd76f48
Xritsos <kobecris24@yahoo.gr>
2024-05-03 18:18:18 +0300

    removed f1 score

--- source/main.py
+++ source/main.py
@@ -20,7 +20,7 @@ def main():
     df = pd.read_csv('/home/akahige/Python Work/Denoising/tests_fully.csv')
     
     test_ids = list(df['test_id'])
-    test_ids = [8]
+    test_ids = [18]
     
     for test_id in test_ids:
         row = int(test_id - 1)
@@ -31,7 +31,6 @@ def main():
         VAL_SIZE = int(df['Val Size'][row])
         TRAIN_SIZE = int(df['Train Size'][row])
         SAVE_PATH = f'/home/akahige/Python Work/Denoising/archive/model_ckpts/fully_cnn_{test_id}/'
-    
         print("================== Parameters ====================")
         print(f"Test Number {test_id}")
         print(f"Epochs: {EPOCHS}")
@@ -57,9 +56,6 @@ def main():
         print("Proceeding with training...")
         
         model = AutoEncoder().to(device)
-        feature_extractor = ViTImageProcessor.from_pretrained('nateraw/vit-base-patch16-224-cifar10')
-        classifier = ViTForImageClassification.from_pretrained('nateraw/vit-base-patch16-224-cifar10').to(device)
-        classifier.eval()
         
         # summary(model, (3, 32, 32))
         # exit()
@@ -85,27 +81,23 @@ def main():
             
             start_train_time = time.time()
             
-            tr_loss, tr_psnr, tr_f1 = train(model, train_loader, optimizer, 
-                                            loss_fn, device, feature_extractor, 
-                                            classifier)
+            tr_loss, tr_psnr = train(model, train_loader, optimizer, 
+                                            loss_fn, device)
             
             end_train_time = time.time()
             train_loss.append(float(tr_loss.cpu()))
             train_psnr.append(float(tr_psnr.cpu()))
-            train_f1.append(float(tr_f1.cpu()))
             
             train_time = round(end_train_time - start_train_time, 2)
             
             start_val_time = time.time()
             
             
-            val_loss, val_psnr, val_f1 = val(model, val_loader, loss_fn, device,
-                                             feature_extractor, classifier)
+            val_loss, val_psnr = val(model, val_loader, loss_fn, device)
             
             end_val_time = time.time()
             validation_loss.append(float(val_loss.cpu()))
             validation_psnr.append(float(val_psnr.cpu()))
-            validation_f1.append(float(val_f1.cpu()))
             
             val_time = round(end_val_time - start_val_time, 2)
             
@@ -115,7 +107,6 @@ def main():
             if epoch == 0:
                 previous_loss.append(val_loss)
                 psnr.append(val_psnr)
-                f1.append(val_f1)
             elif epoch > 0:
                 if val_loss < previous_loss[0]:
                     previous_loss.pop()
@@ -123,9 +114,6 @@ def main():
                     
                     psnr.pop()
                     psnr.append(val_psnr)
-                    
-                    f1.pop()
-                    f1.append(val_f1)
                     
                     torch.save(model.state_dict(), f'{SAVE_PATH}{test_id}.pt')
 
@@ -146,7 +134,6 @@ def main():
         
         df.loc[row, 'Val Loss'] = float(previous_loss[0].cpu())
         df.loc[row, 'Val PSNR'] = float(psnr[0].cpu())
-        df.loc[row, 'Val F1'] = float(f1[0].cpu())
         
         df.to_csv('/home/akahige/Python Work/Denoising/tests_fully.csv', index=False)
         
@@ -174,17 +161,6 @@ def main():
         plt.legend()
         
         plt.savefig(f'{SAVE_PATH}{test_id}_psnr.png')
-        
-        fig = plt.figure(figsize=(10, 10))
-        plt.plot(epochs, train_f1, label='Train F1')
-        plt.plot(epochs, validation_f1, label='Validation F1')
-        
-        plt.xlabel('Number of Epochs')
-        plt.ylabel('F1 Score')
-        plt.suptitle('F1 Metric')
-        plt.legend()
-        
-        plt.savefig(f'{SAVE_PATH}{test_id}_f1.png')
         # plt.show()
     
     
