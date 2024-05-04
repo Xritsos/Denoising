47d1ab6d09dc909833d089afefb25feb5b242e75
Xritsos <kobecris24@yahoo.gr>
2024-05-05 00:08:38 +0300

    new best architecture

--- source/autoencoder_fully.py
+++ source/autoencoder_fully.py
@@ -8,29 +8,29 @@ class AutoEncoder(nn.Module):
         super(AutoEncoder, self).__init__()
         
         # encoder
-        self.enc1 = nn.Conv2d(in_channels=3, out_channels=8, kernel_size=(3, 3), 
+        self.enc1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(3, 3), 
                               stride=(1, 1), padding='same', dilation=1, groups=1, 
                               bias=True, padding_mode='zeros')
         
-        self.batch_enc1 = nn.BatchNorm2d(8, affine=True)
+        self.batch_enc1 = nn.BatchNorm2d(32, affine=True)
         
-        self.enc2 = nn.Conv2d(in_channels=8, out_channels=8, kernel_size=(2, 2), 
+        self.enc2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(2, 2), 
                               stride=(2, 2), padding='valid', dilation=1, groups=1, 
                               bias=True, padding_mode='zeros')
         
-        self.batch_enc2 = nn.BatchNorm2d(8, affine=True)
+        self.batch_enc2 = nn.BatchNorm2d(32, affine=True)
         
-        self.enc3 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3),
+        self.enc3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3),
                               stride=(1, 1), padding='same', dilation=1, groups=1,
                               bias=True, padding_mode='zeros')
         
-        self.batch_enc3 = nn.BatchNorm2d(16, affine=True)
+        self.batch_enc3 = nn.BatchNorm2d(64, affine=True)
         
-        self.enc4 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(2, 2),
+        self.enc4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(2, 2),
                               stride=(2, 2), padding='valid', dilation=1, groups=1,
                               bias=True, padding_mode='zeros')
         
-        self.batch_enc4 = nn.BatchNorm2d(16, affine=True)
+        self.batch_enc4 = nn.BatchNorm2d(64, affine=True)
         
         # self.enc5 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3),
         #                       stride=(1, 1), padding='same', dilation=1, groups=1,
@@ -51,31 +51,31 @@ class AutoEncoder(nn.Module):
         # self.batch_enc7 = nn.BatchNorm2d(8, affine=True)
         
         # decoder 
-        self.dec1 = nn.ConvTranspose2d(in_channels=16, out_channels=16, 
+        self.dec1 = nn.ConvTranspose2d(in_channels=64, out_channels=64, 
                                        kernel_size=(2, 2), stride=(2, 2), 
                                        padding=0, output_padding=0, 
                                        groups=1, bias=True, 
                                        dilation=1, padding_mode='zeros')
         
-        self.batch_dec1 = nn.BatchNorm2d(16, affine=True)
+        self.batch_dec1 = nn.BatchNorm2d(64, affine=True)
         
-        self.dec2 = nn.ConvTranspose2d(in_channels=16, out_channels=8, 
+        self.dec2 = nn.ConvTranspose2d(in_channels=64, out_channels=32, 
                                        kernel_size=(3, 3), stride=(1, 1), 
                                        padding=1, output_padding=0, 
                                        groups=1, bias=True, 
                                        dilation=1, padding_mode='zeros')
         
-        self.batch_dec2 = nn.BatchNorm2d(8, affine=True)
+        self.batch_dec2 = nn.BatchNorm2d(32, affine=True)
         
-        self.dec3 = nn.ConvTranspose2d(in_channels=8, out_channels=8, 
+        self.dec3 = nn.ConvTranspose2d(in_channels=32, out_channels=32, 
                                        kernel_size=(2, 2), stride=(2, 2), 
                                        padding=0, output_padding=0, 
                                        groups=1, bias=True, 
                                        dilation=1, padding_mode='zeros')
         
-        self.batch_dec3 = nn.BatchNorm2d(8, affine=True)
+        self.batch_dec3 = nn.BatchNorm2d(32, affine=True)
         
-        self.dec4 = nn.ConvTranspose2d(in_channels=8, out_channels=3, 
+        self.dec4 = nn.ConvTranspose2d(in_channels=32, out_channels=3, 
                                        kernel_size=(3, 3), stride=(1, 1), 
                                        padding=1, output_padding=0, 
                                        groups=1, bias=True, 
